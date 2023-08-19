303f74644ce0f4cb1c514051c4f62f2363155131
Harry <hazzy995@gmail.com>
2023-08-19 18:28:56 +0100

    fix double line import

--- src/math/index.ts
+++ src/math/index.ts
@@ -2,7 +2,6 @@ import Line from "./line.ts"
 import Rect from './rect.ts';
 import Vector2 from './vector2.ts';
 import Vector3 from './vector3.ts';
-import Line from './line.ts';
 
 export {
     Line,
